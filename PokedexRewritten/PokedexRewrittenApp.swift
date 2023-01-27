633d3399327ebd27814f7f758e4bac14ba565a48
ShaMarcusW-MAC <shamarcus.walker@mobileappscompany.us>
2023-01-27 11:00:04 -0500

    Initial Commit

---
+++ PokedexRewritten/PokedexRewrittenApp.swift
@@ -0,0 +1,17 @@
+//
+//  PokedexRewrittenApp.swift
+//  PokedexRewritten
+//
+//  Created by Consultant on 1/27/23.
+//
+
+import SwiftUI
+
+@main
+struct PokedexRewrittenApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
